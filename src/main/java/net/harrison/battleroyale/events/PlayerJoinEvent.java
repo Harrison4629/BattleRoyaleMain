5cbefc3651dbd652f074deb9ec7fc690248efdcb
Harrison <gamerulekeep1234@gmail.com>
2025-06-07 21:08:34 +0800

    修复由于未加static导致的玩家加入游戏事件onPlayerJoin没正确加载的bug

--- src/main/java/net/harrison/battleroyale/events/PlayerJoinEvent.java
+++ src/main/java/net/harrison/battleroyale/events/PlayerJoinEvent.java
@@ -13,26 +13,32 @@ import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
+import java.util.Objects;
+
 
 @Mod.EventBusSubscriber(modid = Battleroyale.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class PlayerJoinEvent {
     @SubscribeEvent
-    public void onPlayerJoin(PlayerEvent.PlayerLoggedInEvent event) {
+    public static void onPlayerJoin(PlayerEvent.PlayerLoggedInEvent event) {
 
         if (!(event.getEntity() instanceof ServerPlayer player)){
             return;
         }
 
-        if (BattleroyaleManager.getStatus()){
+        Objects.requireNonNull(player.getServer()).execute(() -> {
+            if (BattleroyaleManager.getStatus()){
+                player.setGameMode(GameType.SPECTATOR);
 
-            player.setGameMode(GameType.SPECTATOR);
+                player.connection.send(new ClientboundSetTitleTextPacket(Component.literal("§b您正在观战一场游戏")));
 
-            player.connection.send(new ClientboundSetTitleTextPacket(Component.literal("§b您正在观战一场游戏")));
+                ModMessages.sendToPlayer(new PlaySoundToClientS2CPacket(SoundEvents.VILLAGER_TRADE, 1.0F, 1.0F), player);
+            } else {
+                if (BattleroyaleManager.getHobby() != null) {
+                    player.moveTo(BattleroyaleManager.getHobby());
+                }
+            }
+        });
 
-            ModMessages.sendToPlayer(new PlaySoundToClientS2CPacket(SoundEvents.AMETHYST_BLOCK_PLACE, 1.0F, 1.0F), player);
 
-        } else {
-            player.moveTo(BattleroyaleManager.getHobby());
-        }
     }
 }
